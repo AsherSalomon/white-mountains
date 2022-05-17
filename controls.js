be942a306b332e452b5b99d9f3b108e094a55324
AsherSalomon <AsherSalomon@gmail.com>
2022-05-16 20:30:22 -0400

    Update controls.js

--- controls.js
+++ controls.js
@@ -1,8 +1,19 @@
 
+import { PointerLockControls } from './lib/PointerLockControls.js';
+
 export const Controls = {
   type: 'first person',
-  init: function() {
-    console.log( this.type );
+  init: function( scene ) {
+    controls = new PointerLockControls( camera, document.body );
+    document.body.addEventListener( 'click', function () {
+      if ( controls.isLocked ) {
+        controls.unlock();
+      } else {
+        controls.lock();
+      }
+    }, false );
+
+    scene.add( controls.getObject() );
   },
   animate: function() {
   }
