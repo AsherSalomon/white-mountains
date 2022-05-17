1cc5c308fb1d8dc84cce371755ea1b4815198657
AsherSalomon <AsherSalomon@gmail.com>
2022-05-16 20:30:24 -0400

    Update white-mountains.js

--- white-mountains.js
+++ white-mountains.js
@@ -1,5 +1,4 @@
 import * as THREE from 'three';
-import { PointerLockControls } from './lib/PointerLockControls.js';
 import { Controls } from './controls.js';
 
 var moveForward = false;
@@ -42,38 +41,28 @@ function init() {
 	camera.position.set( 1, 2, - 3 );
 	scene.add( camera );
 
-  controls = new PointerLockControls( camera, document.body );
-  document.body.addEventListener( 'click', function () {
-    if ( controls.isLocked ) {
-      controls.unlock();
-    } else {
-      controls.lock();
-    }
-  }, false );
-
-  scene.add( controls.getObject() );
-  var onKeyDown = function ( event ) {
-    switch ( event.keyCode ) {
-      case 87: moveForward = true; break; // w
-      case 65: moveLeft = true; break; // a
-      case 83: moveBackward = true; break; // s
-      case 68: moveRight = true; break; // d
-      case 32: moveUp = true; break; // space
-      case 16: moveDown = true; break; // shift
-    }
-  };
-  var onKeyUp = function ( event ) {
-    switch ( event.keyCode ) {
-      case 87: moveForward = false; break; // w
-      case 65: moveLeft = false; break; // a
-      case 83: moveBackward = false; break; // s
-      case 68: moveRight = false; break; // d
-      case 32: moveUp = false; break; // space
-      case 16: moveDown = false; break; // shift
-    }
-  };
-  document.addEventListener( 'keydown', onKeyDown, false );
-  document.addEventListener( 'keyup', onKeyUp, false );
+  // var onKeyDown = function ( event ) {
+  //   switch ( event.keyCode ) {
+  //     case 87: moveForward = true; break; // w
+  //     case 65: moveLeft = true; break; // a
+  //     case 83: moveBackward = true; break; // s
+  //     case 68: moveRight = true; break; // d
+  //     case 32: moveUp = true; break; // space
+  //     case 16: moveDown = true; break; // shift
+  //   }
+  // };
+  // var onKeyUp = function ( event ) {
+  //   switch ( event.keyCode ) {
+  //     case 87: moveForward = false; break; // w
+  //     case 65: moveLeft = false; break; // a
+  //     case 83: moveBackward = false; break; // s
+  //     case 68: moveRight = false; break; // d
+  //     case 32: moveUp = false; break; // space
+  //     case 16: moveDown = false; break; // shift
+  //   }
+  // };
+  // document.addEventListener( 'keydown', onKeyDown, false );
+  // document.addEventListener( 'keyup', onKeyUp, false );
 
 	renderer = new THREE.WebGLRenderer( { antialias: true, logarithmicDepthBuffer: true } );
 	renderer.setSize( window.innerWidth, window.innerHeight );
@@ -98,7 +87,7 @@ function onWindowResize() {
 
 function animate() {
 
-	// Controls.animate();
+	Controls.animate();
 
   requestAnimationFrame( animate );
 	renderer.render( scene, camera );
