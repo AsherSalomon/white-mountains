f0f3d24cbe495c1e5f59fbff90202e6d7d6f0a3b
AsherSalomon <AsherSalomon@gmail.com>
2022-05-17 08:36:06 -0400

    Update controls.js

--- controls.js
+++ controls.js
@@ -21,39 +21,39 @@ var leftTouch = { identifier: 0 };
 
 export function init( scene, camera ) {
 
-  controls = new PointerLockControls( camera, document.body );
-  document.body.addEventListener( 'click', function () {
-    if ( controls.isLocked ) {
-      controls.unlock();
-    } else {
-      controls.lock();
-    }
-  }, false );
-
-  scene.add( controls.getObject() );
-
-  function onKeyDown( event ) {
-    switch ( event.keyCode ) {
-      case 87: moveForward = true; break; // w
-      case 65: moveLeft = true; break; // a
-      case 83: moveBackward = true; break; // s
-      case 68: moveRight = true; break; // d
-      case 32: moveUp = true; break; // space
-      case 16: moveDown = true; break; // shift
-    }
-  };
-  function onKeyUp( event ) {
-    switch ( event.keyCode ) {
-      case 87: moveForward = false; break; // w
-      case 65: moveLeft = false; break; // a
-      case 83: moveBackward = false; break; // s
-      case 68: moveRight = false; break; // d
-      case 32: moveUp = false; break; // space
-      case 16: moveDown = false; break; // shift
-    }
-  };
-  document.addEventListener( 'keydown', onKeyDown, false );
-  document.addEventListener( 'keyup', onKeyUp, false );
+  // controls = new PointerLockControls( camera, document.body );
+  // document.body.addEventListener( 'click', function () {
+  //   if ( controls.isLocked ) {
+  //     controls.unlock();
+  //   } else {
+  //     controls.lock();
+  //   }
+  // }, false );
+  //
+  // scene.add( controls.getObject() );
+  //
+  // function onKeyDown( event ) {
+  //   switch ( event.keyCode ) {
+  //     case 87: moveForward = true; break; // w
+  //     case 65: moveLeft = true; break; // a
+  //     case 83: moveBackward = true; break; // s
+  //     case 68: moveRight = true; break; // d
+  //     case 32: moveUp = true; break; // space
+  //     case 16: moveDown = true; break; // shift
+  //   }
+  // };
+  // function onKeyUp( event ) {
+  //   switch ( event.keyCode ) {
+  //     case 87: moveForward = false; break; // w
+  //     case 65: moveLeft = false; break; // a
+  //     case 83: moveBackward = false; break; // s
+  //     case 68: moveRight = false; break; // d
+  //     case 32: moveUp = false; break; // space
+  //     case 16: moveDown = false; break; // shift
+  //   }
+  // };
+  // document.addEventListener( 'keydown', onKeyDown, false );
+  // document.addEventListener( 'keyup', onKeyUp, false );
 
   // https://developer.mozilla.org/en-US/docs/Web/API/Touch_events
 
