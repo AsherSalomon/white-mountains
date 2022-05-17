66f57f41815c7b8c2f5152fd5c5561ebafc01563
AsherSalomon <AsherSalomon@gmail.com>
2022-05-16 20:33:52 -0400

    Update controls.js

--- controls.js
+++ controls.js
@@ -1,11 +1,20 @@
 
 import { PointerLockControls } from './lib/PointerLockControls.js';
 
+let controls;
+
+var moveForward = false;
+var moveBackward = false;
+var moveLeft = false;
+var moveRight = false;
+var moveUp = false;
+var moveDown = false;
+
 export const Controls = {
   type: 'first person',
   init: function( scene, camera ) {
 
-    let controls = new PointerLockControls( camera, document.body );
+    controls = new PointerLockControls( camera, document.body );
     document.body.addEventListener( 'click', function () {
       if ( controls.isLocked ) {
         controls.unlock();
@@ -15,6 +24,29 @@ export const Controls = {
     }, false );
 
     scene.add( controls.getObject() );
+
+    var onKeyDown = function ( event ) {
+      switch ( event.keyCode ) {
+        case 87: moveForward = true; break; // w
+        case 65: moveLeft = true; break; // a
+        case 83: moveBackward = true; break; // s
+        case 68: moveRight = true; break; // d
+        case 32: moveUp = true; break; // space
+        case 16: moveDown = true; break; // shift
+      }
+    };
+    var onKeyUp = function ( event ) {
+      switch ( event.keyCode ) {
+        case 87: moveForward = false; break; // w
+        case 65: moveLeft = false; break; // a
+        case 83: moveBackward = false; break; // s
+        case 68: moveRight = false; break; // d
+        case 32: moveUp = false; break; // space
+        case 16: moveDown = false; break; // shift
+      }
+    };
+    document.addEventListener( 'keydown', onKeyDown, false );
+    document.addEventListener( 'keyup', onKeyUp, false );
 
   },
   animate: function() {
