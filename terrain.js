d62abc185850af4e5c393b38238a0b42a246e2a4
AsherSalomon <AsherSalomon@gmail.com>
2022-05-18 19:06:44 -0400

    Update terrain.js

--- terrain.js
+++ terrain.js
@@ -1,4 +1,18 @@
 
+import proj4 from './lib/proj4.js';
+import tilebelt from './lib/tilebelt.js';
+
+let spacing = 0.05;
+let latitude = 44.2705;
+let longitude = -71.30325;
+
+function snap( n ) {
+  return Math.round( n / spacing ) * spacing;
+}
+
 export function init() {
-  console.log('terrain')
+  let projector = proj4( 'EPSG:3857' );
+  let tile = tilebelt.pointToTileFraction( longitude, latitude, 10 );
+  console.log(tile)
+  // let quadkey = tilebelt.tileToQuadkey( tile );
 }
