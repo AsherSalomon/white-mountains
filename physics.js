1a1db89cc12dcf5d875004b1257a05c1de39d874
AsherSalomon <AsherSalomon@gmail.com>
2022-05-19 08:48:24 -0400

    Update physics.js

--- physics.js
+++ physics.js
@@ -1,5 +1,31 @@
-export let someVariable;
 
-export function setVariable( data ) {
-  someVariable = data;
-}
+// Heightfield parameters
+const terrainWidthExtents = 100;
+const terrainDepthExtents = 100;
+const terrainWidth = 128;
+const terrainDepth = 128;
+const terrainHalfWidth = terrainWidth / 2;
+const terrainHalfDepth = terrainDepth / 2;
+const terrainMaxHeight = 8;
+const terrainMinHeight = - 2;
+
+// Graphics variables
+let terrainMesh;
+const clock = new THREE.Clock();
+
+// Physics variables
+let collisionConfiguration;
+let dispatcher;
+let broadphase;
+let solver;
+let physicsWorld;
+const dynamicObjects = [];
+let transformAux1;
+
+let heightData = null;
+let ammoHeightData = null;
+
+let time = 0;
+const objectTimePeriod = 3;
+let timeNextSpawn = time + objectTimePeriod;
+const maxNumObjects = 30;
