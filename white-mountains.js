0c3c5a6d40da33ad1883b35f6afb5e0cc11d2021
AsherSalomon <AsherSalomon@gmail.com>
2022-05-19 09:32:13 -0400

    Update white-mountains.js

--- white-mountains.js
+++ white-mountains.js
@@ -71,8 +71,8 @@ function animate() {
 
   requestAnimationFrame( animate );
 
-	Physics.render();
-	
+	Physics.render( scene );
+
 	renderer.render( scene, camera );
 
 	Controls.animate( camera );
