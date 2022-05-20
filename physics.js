0fbb38526053889723e0e5db64bc5f4e317b8358
AsherSalomon <AsherSalomon@gmail.com>
2022-05-20 08:05:34 -0400

    Update physics.js

--- physics.js
+++ physics.js
@@ -2,8 +2,8 @@ import * as THREE from 'three';
 import * as Terrain from './terrain.js';
 
 // Heightfield parameters
-const terrainWidthExtents = 100;
-const terrainDepthExtents = 100;
+const terrainWidthExtents = Terrain.tileWidthNS; // 100;
+const terrainDepthExtents = 100; // Terrain.tileWidthEW; // 
 const terrainWidth = Terrain.ELEVATION_TILE_SIZE; // 128;
 const terrainDepth = Terrain.ELEVATION_TILE_SIZE; // 128;
 const terrainHalfWidth = terrainWidth / 2;
