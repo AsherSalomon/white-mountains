f2080ccae1d51b95b757c1dda4d2d9640ca3333b
AsherSalomon <AsherSalomon@gmail.com>
2022-05-20 07:44:22 -0400

    Update physics.js

--- physics.js
+++ physics.js
@@ -1,10 +1,11 @@
 import * as THREE from 'three';
+import * as Terrain from './terrain.js';
 
 // Heightfield parameters
 const terrainWidthExtents = 100;
 const terrainDepthExtents = 100;
-const terrainWidth = 128;
-const terrainDepth = 128;
+const terrainWidth = ELEVATION_TILE_SIZE; // 128;
+const terrainDepth = ELEVATION_TILE_SIZE; // 128;
 const terrainHalfWidth = terrainWidth / 2;
 const terrainHalfDepth = terrainDepth / 2;
 const terrainMaxHeight = 8;
@@ -30,6 +31,46 @@ let time = 0;
 const objectTimePeriod = 3;
 let timeNextSpawn = time + objectTimePeriod;
 const maxNumObjects = 30;
+
+export function genHeight() {
+	heightData = generateHeight( terrainWidth, terrainDepth, terrainMinHeight, terrainMaxHeight );
+}
+
+function generateHeight( width, depth, minHeight, maxHeight ) {
+
+	// Generates the height data (a sinus wave)
+
+	const size = width * depth;
+	const data = new Float32Array( size );
+
+	const hRange = maxHeight - minHeight;
+	const w2 = width / 2;
+	const d2 = depth / 2;
+	const phaseMult = 12;
+
+	let p = 0;
+
+	for ( let j = 0; j < depth; j ++ ) {
+
+		for ( let i = 0; i < width; i ++ ) {
+
+			const radius = Math.sqrt(
+				Math.pow( ( i - w2 ) / w2, 2.0 ) +
+					Math.pow( ( j - d2 ) / d2, 2.0 ) );
+
+			const height = ( Math.sin( radius * phaseMult ) + 1 ) * 0.5 * hRange + minHeight;
+
+			data[ p ] = height;
+
+			p ++;
+
+		}
+
+	}
+
+	return data;
+
+}
 
 export function init( scene ) {
 
@@ -110,46 +151,6 @@ export function initPhysics() {
 	physicsWorld.addRigidBody( groundBody );
 
 	transformAux1 = new Ammo.btTransform();
-
-}
-
-export function genHeight() {
-	heightData = generateHeight( terrainWidth, terrainDepth, terrainMinHeight, terrainMaxHeight );
-}
-
-function generateHeight( width, depth, minHeight, maxHeight ) {
-
-	// Generates the height data (a sinus wave)
-
-	const size = width * depth;
-	const data = new Float32Array( size );
-
-	const hRange = maxHeight - minHeight;
-	const w2 = width / 2;
-	const d2 = depth / 2;
-	const phaseMult = 12;
-
-	let p = 0;
-
-	for ( let j = 0; j < depth; j ++ ) {
-
-		for ( let i = 0; i < width; i ++ ) {
-
-			const radius = Math.sqrt(
-				Math.pow( ( i - w2 ) / w2, 2.0 ) +
-					Math.pow( ( j - d2 ) / d2, 2.0 ) );
-
-			const height = ( Math.sin( radius * phaseMult ) + 1 ) * 0.5 * hRange + minHeight;
-
-			data[ p ] = height;
-
-			p ++;
-
-		}
-
-	}
-
-	return data;
 
 }
 
