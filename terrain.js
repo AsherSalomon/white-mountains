fc442ced8dfad00bece2f150ffbca001aa576a7d
AsherSalomon <AsherSalomon@gmail.com>
2022-05-21 06:55:40 -0400

    Update terrain.js

--- terrain.js
+++ terrain.js
@@ -38,6 +38,11 @@ export function extablishScale() {
   tileWidthEW = earthsRaius * deltaEW * Math.PI / 180;
 }
 
+function dataToHeight( data ) {
+  // Elevation in meters
+  return -10000 + ( data[ 0 ] * 65536 + data[ 1 ] * 256 + data[ 2 ] ) * 0.1;
+}
+
 export function loadTile() {
   if ( z < 0 || z > maxZoom['terrain'] ) {
     console.error('z < 0 || z > maxZoom');
