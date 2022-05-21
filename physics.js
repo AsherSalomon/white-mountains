14c2eeb959a23abb81086455a5c7a1deddad6657
AsherSalomon <AsherSalomon@gmail.com>
2022-05-21 06:41:39 -0400

    Update physics.js

--- physics.js
+++ physics.js
@@ -1,14 +1,14 @@
 import * as THREE from 'three';
 
-// // Heightfield parameters
-// let terrainWidthExtents; // Terrain.tileWidthNS; //  = 100
-// let terrainDepthExtents; // Terrain.tileWidthEW; // = 100
-// const terrainWidth = // 128;
-// const terrainDepth = // 128;
-// const terrainHalfWidth = terrainWidth / 2;
-// const terrainHalfDepth = terrainDepth / 2;
-// const terrainMaxHeight = 1916.5824; // i.e. 6,288'
-// const terrainMinHeight = 0;
+// Heightfield parameters
+let terrainWidthExtents; // Terrain.tileWidthNS; //  = 100
+let terrainDepthExtents; // Terrain.tileWidthEW; // = 100
+const terrainWidth = 128;
+const terrainDepth = 128;
+const terrainHalfWidth = terrainWidth / 2;
+const terrainHalfDepth = terrainDepth / 2;
+const terrainMaxHeight = 1916.5824; // i.e. 6,288'
+const terrainMinHeight = 0;
 //
 // // Graphics variables
 // let terrainMesh;
@@ -24,9 +24,9 @@ const dynamicObjects = [];
 let transformAux1;
 //
 // let heightData = null;
-// let ammoHeightData = null;
+let ammoHeightData = null;
 //
-// let time = 0;
+let time = 0;
 // const objectTimePeriod = 3;
 // let timeNextSpawn = time + objectTimePeriod;
 // const maxNumObjects = 30;
@@ -42,20 +42,83 @@ export function initPhysics() {
 	physicsWorld = new Ammo.btDiscreteDynamicsWorld( dispatcher, broadphase, solver, collisionConfiguration );
 	physicsWorld.setGravity( new Ammo.btVector3( 0, - 6, 0 ) );
 
-	// Create the terrain body
-
-	// const groundShape = createTerrainShape();
-	// const groundTransform = new Ammo.btTransform();
-	// groundTransform.setIdentity();
-	// // Shifts the terrain, since bullet re-centers it on its bounding box.
-	// groundTransform.setOrigin( new Ammo.btVector3( 0, ( terrainMaxHeight + terrainMinHeight ) / 2, 0 ) );
-	// const groundMass = 0;
-	// const groundLocalInertia = new Ammo.btVector3( 0, 0, 0 );
-	// const groundMotionState = new Ammo.btDefaultMotionState( groundTransform );
-	// const groundBody = new Ammo.btRigidBody( new Ammo.btRigidBodyConstructionInfo( groundMass, groundMotionState, groundShape, groundLocalInertia ) );
-	// physicsWorld.addRigidBody( groundBody );
-
 	transformAux1 = new Ammo.btTransform();
+
+}
+
+export function createTerrainBody( heightData ) {
+
+  const groundShape = createTerrainShape( heightData );
+  const groundTransform = new Ammo.btTransform();
+  groundTransform.setIdentity();
+  // Shifts the terrain, since bullet re-centers it on its bounding box.
+  groundTransform.setOrigin( new Ammo.btVector3( 0, ( terrainMaxHeight + terrainMinHeight ) / 2, 0 ) );
+  const groundMass = 0;
+  const groundLocalInertia = new Ammo.btVector3( 0, 0, 0 );
+  const groundMotionState = new Ammo.btDefaultMotionState( groundTransform );
+  const groundBody = new Ammo.btRigidBody( new Ammo.btRigidBodyConstructionInfo( groundMass, groundMotionState, groundShape, groundLocalInertia ) );
+  physicsWorld.addRigidBody( groundBody );
+
+}
+
+function createTerrainShape( heightData ) {
+
+	// This parameter is not really used, since we are using PHY_FLOAT height data type and hence it is ignored
+	const heightScale = 1;
+
+	// Up axis = 0 for X, 1 for Y, 2 for Z. Normally 1 = Y is used.
+	const upAxis = 1;
+
+	// hdt, height data type. "PHY_FLOAT" is used. Possible values are "PHY_FLOAT", "PHY_UCHAR", "PHY_SHORT"
+	const hdt = 'PHY_FLOAT';
+
+	// Set this to your needs (inverts the triangles)
+	const flipQuadEdges = false;
+
+	// Creates height data buffer in Ammo heap
+	ammoHeightData = Ammo._malloc( 4 * terrainWidth * terrainDepth );
+
+	// Copy the javascript height data array to the Ammo one.
+	let p = 0;
+	let p2 = 0;
+
+	for ( let j = 0; j < terrainDepth; j ++ ) {
+
+		for ( let i = 0; i < terrainWidth; i ++ ) {
+
+			// write 32-bit float data to memory
+			Ammo.HEAPF32[ ammoHeightData + p2 >> 2 ] = heightData[ p ];
+
+			p ++;
+
+			// 4 bytes/float
+			p2 += 4;
+
+		}
+
+	}
+
+	// Creates the heightfield physics shape
+	const heightFieldShape = new Ammo.btHeightfieldTerrainShape(
+		terrainWidth,
+		terrainDepth,
+		ammoHeightData,
+		heightScale,
+		terrainMinHeight,
+		terrainMaxHeight,
+		upAxis,
+		hdt,
+		flipQuadEdges
+	);
+
+	// Set horizontal scale
+	const scaleX = terrainWidthExtents / ( terrainWidth - 1 );
+	const scaleZ = terrainDepthExtents / ( terrainDepth - 1 );
+	heightFieldShape.setLocalScaling( new Ammo.btVector3( scaleX, 1, scaleZ ) );
+
+	heightFieldShape.setMargin( 0.05 );
+
+	return heightFieldShape;
 
 }
 
