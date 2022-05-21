5eea6d09b4268cc3bfd19668659004e167c49515
AsherSalomon <AsherSalomon@gmail.com>
2022-05-20 22:44:52 -0400

    Update physics.js

--- physics.js
+++ physics.js
@@ -32,8 +32,9 @@ const objectTimePeriod = 3;
 let timeNextSpawn = time + objectTimePeriod;
 const maxNumObjects = 30;
 
-function callback() {
-	Physics.genHeight();
+function callback( data ) {
+	// Physics.genHeight();
+  heightData = data;
 	Physics.init();
 	Physics.initPhysics();
 }
@@ -42,45 +43,45 @@ export function main() {
 	Terrain.loadData( callback );
 }
 
-export function genHeight() {
-	heightData = generateHeight( terrainWidth, terrainDepth, terrainMinHeight, terrainMaxHeight );
-}
+// export function genHeight() {
+// 	heightData = generateHeight( terrainWidth, terrainDepth, terrainMinHeight, terrainMaxHeight );
+// }
 
-function generateHeight( width, depth, minHeight, maxHeight ) {
-
-	// Generates the height data (a sinus wave)
-
-	const size = width * depth;
-	const data = new Float32Array( size );
-
-	const hRange = maxHeight - minHeight;
-	const w2 = width / 2;
-	const d2 = depth / 2;
-	const phaseMult = 12;
-
-	let p = 0;
-
-	for ( let j = 0; j < depth; j ++ ) {
-
-		for ( let i = 0; i < width; i ++ ) {
-
-			const radius = Math.sqrt(
-				Math.pow( ( i - w2 ) / w2, 2.0 ) +
-					Math.pow( ( j - d2 ) / d2, 2.0 ) );
-
-			const height = ( Math.sin( radius * phaseMult ) + 1 ) * 0.5 * hRange + minHeight;
-
-			data[ p ] = height;
-
-			p ++;
-
-		}
-
-	}
-
-	return data;
-
-}
+// function generateHeight( width, depth, minHeight, maxHeight ) {
+//
+// 	// Generates the height data (a sinus wave)
+//
+// 	const size = width * depth;
+// 	const data = new Float32Array( size );
+//
+// 	const hRange = maxHeight - minHeight;
+// 	const w2 = width / 2;
+// 	const d2 = depth / 2;
+// 	const phaseMult = 12;
+//
+// 	let p = 0;
+//
+// 	for ( let j = 0; j < depth; j ++ ) {
+//
+// 		for ( let i = 0; i < width; i ++ ) {
+//
+// 			const radius = Math.sqrt(
+// 				Math.pow( ( i - w2 ) / w2, 2.0 ) +
+// 					Math.pow( ( j - d2 ) / d2, 2.0 ) );
+//
+// 			const height = ( Math.sin( radius * phaseMult ) + 1 ) * 0.5 * hRange + minHeight;
+//
+// 			data[ p ] = height;
+//
+// 			p ++;
+//
+// 		}
+//
+// 	}
+//
+// 	return data;
+//
+// }
 
 export function init() {
 
