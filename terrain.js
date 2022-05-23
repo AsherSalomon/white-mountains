596d5e2bfd029b82bfed44f943b9ed999f407499
AsherSalomon <AsherSalomon@gmail.com>
2022-05-23 08:31:55 -0400

    Update terrain.js

--- terrain.js
+++ terrain.js
@@ -91,7 +91,6 @@ export function loadTexture( callback, z ) {
       const ctx = satelliteCanvas.getContext( '2d' );
       ctx.drawImage( image, 0, 0 );
       let texture = new CanvasTexture( satelliteCanvas );
-      console.log(texture);
       callback( texture );
     },
     undefined, // onProgress not supported
