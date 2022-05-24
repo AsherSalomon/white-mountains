288e1326fb2d9507595dade8782358a86c19c41c
AsherSalomon <AsherSalomon@gmail.com>
2022-05-23 21:57:52 -0400

    Update terrain.js

--- terrain.js
+++ terrain.js
@@ -3,6 +3,19 @@ import * as tilebelt from './lib/tilebelt.js';
 let latitude = 44.2705; // Mt. Washington
 let longitude = -71.30325;
 
-export function seedGrid() {
+let grid = [];
 
+function Tile(name, x, y) {
+  this.name = name;
+  this.x = x;
+  this.y = y;
+  this.log = function( logMe ) {
+    console.log( this.name );
+    console.log( logMe );
+  };
+}
+
+export function seedGrid() {
+  a.push( new tile( 'hello', 0, 0 ) );
+  a[ 0 ].log( 'asher' );
 }
