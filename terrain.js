6fdf0c9c96d63c7d95ed6e2d5e8992ae417d607f
AsherSalomon <AsherSalomon@gmail.com>
2022-05-23 22:10:15 -0400

    Update terrain.js

--- terrain.js
+++ terrain.js
@@ -2,6 +2,7 @@ import * as tilebelt from './lib/tilebelt.js';
 
 let latitude = 44.2705; // Mt. Washington
 let longitude = -71.30325;
+let earthsRaius = 6371000; // meters
 
 let tileWidthNS;
 let tileWidthEW;
