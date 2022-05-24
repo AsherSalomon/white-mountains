75905aaffbe7bace2904d15a139bb363d36ef2eb
AsherSalomon <AsherSalomon@gmail.com>
2022-05-23 22:17:02 -0400

    Update terrain.js

--- terrain.js
+++ terrain.js
@@ -3,6 +3,7 @@ import * as tilebelt from './lib/tilebelt.js';
 let latitude = 44.2705; // Mt. Washington
 let longitude = -71.30325;
 let earthsRaius = 6371000; // meters
+let tileWidth; // 6999.478360682135 meters
 
 let maxZoom = {
   terrain: 12,
@@ -25,8 +26,11 @@ export function seed() {
   let deltaEW = bbox[2] - bbox[0]; // e - w
   let tileWidthNS = earthsRaius * deltaNS * Math.PI / 180;
   let tileWidthEW = earthsRaius * deltaEW * Math.PI / 180 * Math.cos( latitude * Math.PI / 180 );
-  let tileWidth = ( tileWidthNS + tileWidthEW ) / 2;
-  console.log( tileWidth );
+  tileWidth = ( tileWidthNS + tileWidthEW ) / 2;
+
+  let tileSeed = tilebelt.pointToTileFraction( longitude, latitude, maxZoom['terrain'] );
+  let seedName = tilebelt.tileToQuadkey( tileSeed );
+  console.log( seedName );
   // grid.push( new Tile( 'hello', 0, 0 ) );
   // grid[ 0 ].log( 'asher' );
 }
