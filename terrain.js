6ff88359ab638264bb463a0e08aecef8e47ec787
AsherSalomon <AsherSalomon@gmail.com>
2022-05-23 22:09:17 -0400

    Update terrain.js

--- terrain.js
+++ terrain.js
@@ -3,22 +3,36 @@ import * as tilebelt from './lib/tilebelt.js';
 let latitude = 44.2705; // Mt. Washington
 let longitude = -71.30325;
 
+let tileWidthNS;
+let tileWidthEW;
+
+let maxZoom = {
+  terrain: 12,
+  satellite: 20
+}
+
 let grid = [];
 
-function Tile(name, x, y) {
+function Tile( name ) {
   this.name = name;
-  this.x = x;
-  this.y = y;
-  this.log = function( logMe ) {
+  this.log = function() {
     console.log( this.name );
-    console.log( logMe );
   };
 }
 
-export function seedGrid() {
-  grid.push( new Tile( 'hello', 0, 0 ) );
-  grid[ 0 ].log( 'asher' );
-  grid.push( new Tile( 'goodby', 0, 0 ) );
-  grid[ 1 ].log( 'jayden' );
-  grid[ 0 ].log( 'mai' );
+export function seed() {
+  let tile = tilebelt.pointToTile( longitude, latitude, maxZoom['terrain'] );
+  let bbox = tilebelt.tileToBBOX( tile ); // [w, s, e, n]
+  let deltaNS = bbox[3] - bbox[1]; // n - s
+  let deltaEW = bbox[2] - bbox[0]; // e - w
+  tileWidthNS = earthsRaius * deltaNS * Math.PI / 180;
+  tileWidthEW = earthsRaius * deltaEW * Math.PI / 180 * Math.cos( latitude * Math.PI / 180 );
+  console.log( tileWidthNS );
+  console.log( tileWidthEW );
+  // grid.push( new Tile( 'hello', 0, 0 ) );
+  // grid[ 0 ].log( 'asher' );
+}
+
+export function update() {
+
 }
