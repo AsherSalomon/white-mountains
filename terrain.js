5de701e9bf0998d318e41f063b54f09940338dea
AsherSalomon <AsherSalomon@gmail.com>
2022-05-26 22:42:48 -0400

    Update terrain.js

--- terrain.js
+++ terrain.js
@@ -105,7 +105,7 @@ class Tile {
   }
   isTooSmall() {
     let tooSmall = this.width / this.distanceFromCamera() < angularResolution / 2;
-    return tooSmall || frustum.intersectsBox( this.boundingBox ) == false;
+    return tooSmall; // || frustum.intersectsBox( this.boundingBox ) == false;
   }
   allSmall() {
     let allSiblingsAreSmall = false;
