11888cfa9be5363730c4b536d4013c7be180008a
AsherSalomon <AsherSalomon@gmail.com>
2022-05-26 21:36:16 -0400

    Update terrain.js

--- terrain.js
+++ terrain.js
@@ -153,11 +153,11 @@ class Tile {
       	const size = ELEVATION_TILE_SIZE * ELEVATION_TILE_SIZE;
       	const heightData = new Float32Array( size );
 
-        
+
         try {
           this.dataToHeight( [ 1, 2 ,3 ] );
         } catch (error) {
-          console.error(error);
+          console.error('wtf');
         }
 
         for ( let i = 0; i < size; i++ ) {
