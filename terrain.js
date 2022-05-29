5abf573d082bf6fd9e15e476d70e1ed8ad9c2542
AsherSalomon <AsherSalomon@gmail.com>
2022-05-28 22:38:17 -0400

    Update terrain.js

--- terrain.js
+++ terrain.js
@@ -23,9 +23,9 @@ const IMAGERY_TILE_SIZE = 256;
 let grid = [];
 
 let terrainWorker = new Worker('terrainWorker.js');
-terrainWorker.onmessage = function(event) {
-  console.log( event.data );
-};
+// terrainWorker.onmessage = function( event ) {
+//   console.log( event.data );
+// };
 terrainWorker.postMessage( 'hello asher' );
 
 let apiKey = '5oT5Np7ipsbVhre3lxdi';
