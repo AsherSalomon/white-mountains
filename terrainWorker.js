93ff51bd9c64f263ed60eadfa0db87c90c034e29
AsherSalomon <AsherSalomon@gmail.com>
2022-05-29 10:54:48 -0400

    Update terrainWorker.js

--- terrainWorker.js
+++ terrainWorker.js
@@ -2,6 +2,8 @@
 // https://www.w3schools.com/html/html5_webworkers.asp
 // https://developer.mozilla.org/en-US/docs/Web/API/Web_Workers_API/Using_web_workers
 // https://developer.mozilla.org/en-US/docs/Web/API/Worker/Worker
+// https://developer.mozilla.org/en-US/docs/Web/API/Worker/postMessage
+// https://developer.mozilla.org/en-US/docs/Web/API/Web_Workers_API/Structured_clone_algorithm
 
 // var i = 0;
 //
