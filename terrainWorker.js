d98e3195a63780a248be85d0a8da842374742d8d
AsherSalomon <AsherSalomon@gmail.com>
2022-05-28 22:37:22 -0400

    Update terrainWorker.js

--- terrainWorker.js
+++ terrainWorker.js
@@ -2,15 +2,15 @@
 // https://www.w3schools.com/html/html5_webworkers.asp
 // https://developer.mozilla.org/en-US/docs/Web/API/Web_Workers_API/Using_web_workers
 
-// var i = 0;
-//
-// function timedCount() {
-//   i = i + 1;
-//   postMessage(i);
-//   setTimeout("timedCount()",500);
-// }
-//
-// timedCount();
+var i = 0;
+
+function timedCount() {
+  i = i + 1;
+  postMessage(i);
+  setTimeout("timedCount()",500);
+}
+
+timedCount();
 
 onmessage = function(e) {
   console.log( e.data );
