e041d7b56f7efd3ee46d65e62d3c483e87512f0d
AsherSalomon <AsherSalomon@gmail.com>
2022-05-29 11:01:05 -0400

    Update terrainWorker.js

--- terrainWorker.js
+++ terrainWorker.js
@@ -15,6 +15,18 @@
 //
 // timedCount();
 
+// onmessage = function( event ) {
+//   console.log( event.data );
+}
+
+const earthsRaius = 6371000;
+
 onmessage = function( event ) {
-  console.log( event.data );
+  let imageData = event.data[ 0 ];
+  let vertices = event.data[ 1 ];
+  let curvatureOfTheEarth;
+  for ( let i = 0, j = 0, l = vertices.length; i < l; i ++, j += 3 ) {
+    curvatureOfTheEarth = ( Math.pow( vertices[ j + 0 ], 2 ) + Math.pow( vertices[ j + 2 ], 2 ) ) / ( 2 * earthsRaius );
+    vertices[ j + 1 ] = heightData[ i ] - curvatureOfTheEarth;
+  }
 }
