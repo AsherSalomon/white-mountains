266c0759596178356745c87efbe9c1b4d15f81cc
AsherSalomon <AsherSalomon@gmail.com>
2022-05-31 21:42:35 -0400

    Update white-mountains.js

--- white-mountains.js
+++ white-mountains.js
@@ -65,43 +65,29 @@ function init() {
 	renderer.domElement.style.userSelect = 'none';
 
 	// Skybox
-
 	sun = new THREE.Vector3( 0, 1, 1 );
-
 	const sky = new Sky();
 	sky.scale.setScalar( 341462 );
 	scene.add( sky );
-
 	const skyUniforms = sky.material.uniforms;
-
 	skyUniforms[ 'turbidity' ].value = 10;
 	skyUniforms[ 'rayleigh' ].value = 2;
 	skyUniforms[ 'mieCoefficient' ].value = 0.005;
 	skyUniforms[ 'mieDirectionalG' ].value = 0.8;
-
 	const parameters = {
 		elevation: 45,
 		azimuth: 0
 	};
-
 	const pmremGenerator = new THREE.PMREMGenerator( renderer );
-
 	function updateSun() {
-
 		const phi = THREE.MathUtils.degToRad( 90 - parameters.elevation );
 		const theta = THREE.MathUtils.degToRad( parameters.azimuth );
-
 		sun.setFromSphericalCoords( 1, phi, theta );
-
 		sky.material.uniforms[ 'sunPosition' ].value.copy( sun );
 		// water.material.uniforms[ 'sunDirection' ].value.copy( sun ).normalize();
-
 		scene.environment = pmremGenerator.fromScene( sky ).texture;
-
 	}
-
 	updateSun();
-
 	// end Skybox
 
 	Controls.init( scene, camera );
