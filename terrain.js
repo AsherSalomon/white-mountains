35d6ff0f4e7fc353eee8726f38a56126b8c4295e
AsherSalomon <AsherSalomon@gmail.com>
2022-06-02 20:44:53 -0400

    Update terrain.js

--- terrain.js
+++ terrain.js
@@ -66,7 +66,7 @@ class Tile {
       let dist = this.width / 2;
   		this.clipPlanes = [
   			new THREE.Plane( new THREE.Vector3( 1, 0, 0 ), -centerX - dist ),
-  			new THREE.Plane( new THREE.Vector3( -1, 0, 0 ), -centerX - dist ),
+  			new THREE.Plane( new THREE.Vector3( -1, 0, 0 ), centerX - dist ),
   			new THREE.Plane( new THREE.Vector3( 0, 0, 1 ), 0 - dist ),
   			new THREE.Plane( new THREE.Vector3( 0, 0, - 1 ), 0 - dist )
   		];
