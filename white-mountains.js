57e4a09ad7c167e680b144fd9ca8a05acdf209f4
AsherSalomon <AsherSalomon@gmail.com>
2022-06-02 21:07:16 -0400

    Update white-mountains.js

--- white-mountains.js
+++ white-mountains.js
@@ -36,8 +36,8 @@ function init() {
 	// scene.background = new THREE.Color( 0x2759b0 );
 	scene.fog = new THREE.Fog( 0x7f7f7f, 1, 156260 );
 
-	// const axesHelper = new THREE.AxesHelper( 1609.34 ); // 1 mile
-	// scene.add( axesHelper );
+	const axesHelper = new THREE.AxesHelper( 1609.34 ); // 1 mile
+	scene.add( axesHelper );
 
 	camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, NEAR, FAR );
 	scene.add( camera );
