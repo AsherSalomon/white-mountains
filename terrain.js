977a964b241615955c5beaed9de88f2d5531408d
AsherSalomon <AsherSalomon@gmail.com>
2022-06-02 22:06:24 -0400

    Update terrain.js

--- terrain.js
+++ terrain.js
@@ -276,9 +276,10 @@ export function update() {
 
   for ( let i = 0; i < grid.length; i++ ) {
     grid[ i ].update();
-    // if ( grid[ i ].geometry != null ) {
-    //   grid[ i ].geometry.translate( 1, 0, 0 );
-    // }
+    if ( grid[ i ].geometry != null ) {
+      // grid[ i ].geometry.translate( 1, 0, 0 );
+      grid[ i ].geometry.scale( 1.1, 1, 1 );
+    }
   }
 
   updateGeneratorQueue();
