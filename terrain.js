4cd806efd3ad17b91e3cf41bad8e33c592df4a0a
AsherSalomon <AsherSalomon@gmail.com>
2022-06-03 06:40:53 -0400

    Update terrain.js

--- terrain.js
+++ terrain.js
@@ -65,6 +65,17 @@ class Tile {
     this.centerX = ( 0.5 + this.tile[ 0 ] - this.origin[ 0 ] ) * this.width;
     this.centerZ = ( 0.5 + this.tile[ 1 ] - this.origin[ 1 ] ) * this.width;
   }
+  setClippingPlanes() {
+    this.clipPlanes = [
+      new THREE.Plane( new THREE.Vector3( 1, 0, 0 ), -this.centerX - this.width / 2 ),
+      new THREE.Plane( new THREE.Vector3( -1, 0, 0 ), this.centerX - this.width / 2 ),
+      new THREE.Plane( new THREE.Vector3( 0, 0, 1 ), -this.centerZ - this.width / 2 ),
+      new THREE.Plane( new THREE.Vector3( 0, 0, - 1 ), this.centerZ - this.width / 2 )
+    ];
+    if ( moveToNewTile && this.parent.groundMaterial != null ) {
+      this.parent.groundMaterial.clippingPlanes = this.clipPlanes;
+    }
+  }
   update() {
     // let centerX = ( 0.5 + this.tile[ 0 ] - this.origin[ 0 ] ) * this.width;
     // let centerZ = ( 0.5 + this.tile[ 1 ] - this.origin[ 1 ] ) * this.width;
@@ -86,15 +97,7 @@ class Tile {
       // centerX = ( 0.5 + this.tile[ 0 ] - this.origin[ 0 ] ) * this.width;
       // centerZ = ( 0.5 + this.tile[ 1 ] - this.origin[ 1 ] ) * this.width;
       this.reCenter();
-  		this.clipPlanes = [
-  			new THREE.Plane( new THREE.Vector3( 1, 0, 0 ), -this.centerX - this.width / 2 ),
-  			new THREE.Plane( new THREE.Vector3( -1, 0, 0 ), this.centerX - this.width / 2 ),
-  			new THREE.Plane( new THREE.Vector3( 0, 0, 1 ), -this.centerZ - this.width / 2 ),
-  			new THREE.Plane( new THREE.Vector3( 0, 0, - 1 ), this.centerZ - this.width / 2 )
-  		];
-      if ( moveToNewTile && this.parent.groundMaterial != null ) {
-        this.parent.groundMaterial.clippingPlanes = this.clipPlanes;
-      }
+  		this.setClippingPlanes();
 
       this.inScene = true;
       this.loading = true;
