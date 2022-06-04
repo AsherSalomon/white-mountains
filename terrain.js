849e6f309c922a1bef73a2c6f842798cf72b569b
AsherSalomon <AsherSalomon@gmail.com>
2022-06-04 10:06:51 -0400

    Update terrain.js

--- terrain.js
+++ terrain.js
@@ -281,11 +281,7 @@ class Tile {
       let d2 = d12 + ( d22 - d12 ) * ( m - m1 );
       let interpolated = d1 + ( d2 - d1 ) * ( n - n1 );
       if ( isNaN( interpolated ) == false ) {
-        if ( this.z == maxZoom['terrain'] - 1 ) {
-          return interpolated;
-        } else {
-          return 0;
-        }
+        return interpolated;
       } else {
         return 0;
       }
