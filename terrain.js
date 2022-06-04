ef35ce4fe8a420801c547e667edea36ab15e4b4d
AsherSalomon <AsherSalomon@gmail.com>
2022-06-04 09:24:41 -0400

    Update terrain.js

--- terrain.js
+++ terrain.js
@@ -262,8 +262,8 @@ class Tile {
     this.groundMaterial.needsUpdate = true;
   }
   lookupData( x, z ) {
-    let m = ( x - ( this.centerX - this.width / 2 ) ) / this.width * ( ELEVATION_TILE_SIZE + 1 );
-    let n = ( z - ( this.centerZ - this.width / 2 ) ) / this.width * ( ELEVATION_TILE_SIZE + 1 );
+    let m = ( x - ( this.centerX ) ) / this.width * ( ELEVATION_TILE_SIZE + 1 );
+    let n = ( z - ( this.centerZ ) ) / this.width * ( ELEVATION_TILE_SIZE + 1 );
     if ( m > 0 && n > 0 && m < ELEVATION_TILE_SIZE + 1 && n < ELEVATION_TILE_SIZE + 1 ) {
       let m1 = Math.floor( m );
       let m2 = Math.ceil( m );
