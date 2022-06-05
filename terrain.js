587de7723aeefa26f56c9ae904f9c5de477fe40b
AsherSalomon <AsherSalomon@gmail.com>
2022-06-04 21:24:29 -0400

    Update terrain.js

--- terrain.js
+++ terrain.js
@@ -184,7 +184,7 @@ class Tile {
         let mIsEdge = m == 0 || m == ELEVATION_TILE_SIZE;
         let nIsEdge = n == 0 || n == ELEVATION_TILE_SIZE;
         if ( !mIsEdge && !nIsEdge ) {
-          vertices[ j + 1 ] = this.heightData[ i ] - curvatureOfTheEarth + i;
+          vertices[ j + 1 ] = this.heightData[ i ] - curvatureOfTheEarth;
         } else if ( this.parent != null ) {
           vertices[ j + 1 ] = this.parent.lookupData( x, z ) - curvatureOfTheEarth;
         } else {
