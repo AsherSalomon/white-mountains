c9af668d538a70b8cd4ef35fb37a8657bdc4ca8d
AsherSalomon <AsherSalomon@gmail.com>
2022-06-04 22:46:35 -0400

    Update terrain.js

--- terrain.js
+++ terrain.js
@@ -264,23 +264,23 @@ class Tile {
     let m = ( z - ( this.centerZ - this.width / 2 ) ) / this.width * ( ELEVATION_TILE_SIZE );
     let n = ( x - ( this.centerX - this.width / 2 ) ) / this.width * ( ELEVATION_TILE_SIZE );
     if ( m > 0 && n > 0 && m < ELEVATION_TILE_SIZE - 1 && n < ELEVATION_TILE_SIZE - 1 ) {
-      // let m1 = Math.floor( m );
-      // let m2 = Math.ceil( m );
-      // let n1 = Math.floor( n );
-      // let n2 = Math.ceil( n );
-      // let i11 = m1 * ELEVATION_TILE_SIZE + n1;
-      // let i21 = m2 * ELEVATION_TILE_SIZE + n1;
-      // let i12 = m1 * ELEVATION_TILE_SIZE + n2;
-      // let i22 = m2 * ELEVATION_TILE_SIZE + n2;
-      // let d11 = this.heightData[ i11 ];
-      // let d21 = this.heightData[ i21 ];
-      // let d12 = this.heightData[ i12 ];
-      // let d22 = this.heightData[ i22 ];
-      // let d1 = d11 + ( d21 - d11 ) * ( m - m1 );
-      // let d2 = d12 + ( d22 - d12 ) * ( m - m1 );
-      // let interpolated = d1 + ( d2 - d1 ) * ( n - n1 );
-      // return interpolated;
-      return this.heightData[ Math.round( m ) * ELEVATION_TILE_SIZE + Math.round( n ) ];
+      let m1 = Math.floor( m );
+      let m2 = Math.ceil( m );
+      let n1 = Math.floor( n );
+      let n2 = Math.ceil( n );
+      let i11 = m1 * ELEVATION_TILE_SIZE + n1;
+      let i21 = m2 * ELEVATION_TILE_SIZE + n1;
+      let i12 = m1 * ELEVATION_TILE_SIZE + n2;
+      let i22 = m2 * ELEVATION_TILE_SIZE + n2;
+      let d11 = this.heightData[ i11 ];
+      let d21 = this.heightData[ i21 ];
+      let d12 = this.heightData[ i12 ];
+      let d22 = this.heightData[ i22 ];
+      let d1 = d11 + ( d21 - d11 ) * ( m - m1 );
+      let d2 = d12 + ( d22 - d12 ) * ( m - m1 );
+      let interpolated = d1 + ( d2 - d1 ) * ( n - n1 );
+      return interpolated;
+      // return this.heightData[ Math.round( m ) * ELEVATION_TILE_SIZE + Math.round( n ) ];
     } else if ( this.parent != null ) {
       return this.parent.lookupData( x, z );
     } else {
