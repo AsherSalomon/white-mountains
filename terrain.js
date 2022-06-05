6001f2028b0a98c5d5cd94467f178185441f66ff
AsherSalomon <AsherSalomon@gmail.com>
2022-06-04 21:12:34 -0400

    Update terrain.js

--- terrain.js
+++ terrain.js
@@ -283,7 +283,7 @@ class Tile {
       if ( isNaN( interpolated ) == false ) {
         return interpolated;
       } else {
-        console.log( [ d1, d2, d1, n, n1 ] );
+        console.log( [ d11, d21, d11, m, m1 ] );
         return 0;
       }
     } else if ( this.parent != null ) {
