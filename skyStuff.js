f23cec53761509b8a632653b95c2d434a37331eb
AsherSalomon <AsherSalomon@gmail.com>
2022-06-07 21:27:33 -0400

    Update skyStuff.js

--- skyStuff.js
+++ skyStuff.js
@@ -1,10 +1,12 @@
+
+// https://threejs.org/examples/#webgl_shaders_ocean
+
 import * as THREE from 'three';
 import { Sky } from './lib/Sky.js';
 
 let sun;
 
 export function init( scene, renderer ) {
-  // Skybox
   sun = new THREE.Vector3();
   const sky = new Sky();
   sky.scale.setScalar( 341462 * 1000 );
@@ -24,9 +26,7 @@ export function init( scene, renderer ) {
   	const theta = THREE.MathUtils.degToRad( parameters.azimuth );
   	sun.setFromSphericalCoords( 1, phi, theta );
   	sky.material.uniforms[ 'sunPosition' ].value.copy( sun );
-  	// water.material.uniforms[ 'sunDirection' ].value.copy( sun ).normalize();
   	scene.environment = pmremGenerator.fromScene( sky ).texture;
   }
   updateSun();
-  // end Skybox
 }
