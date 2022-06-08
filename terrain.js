d9320f7578a887121c21a4f4273fc471a03f8763
AsherSalomon <AsherSalomon@gmail.com>
2022-06-07 23:20:05 -0400

    Update terrain.js

--- terrain.js
+++ terrain.js
@@ -210,7 +210,7 @@ class Tile {
 
     if ( this.groundMaterial == null ) {
       this.groundMaterial = new THREE.MeshStandardMaterial( {
-        roughness: 0.8,
+        roughness: 0.5,
         clipIntersection: true,
         color: pineGreen
       } );
