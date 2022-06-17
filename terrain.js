4503f02ae3ae5c9db49c56ac87f1c28cdccf70ac
AsherSalomon <AsherSalomon@gmail.com>
2022-06-17 08:19:41 -0400

    Update terrain.js

--- terrain.js
+++ terrain.js
@@ -271,7 +271,7 @@ class ReusedMesh {
       undefined, // onProgress not supported
       function () {
         console.log( 'terrain ImageLoader error' );
-        console.log( THREE.Cache.files[ url ] );
+        console.log( THREE.Cache.files );
         // console.log( THREE.Cache.remove( url ) );
         // console.log( THREE.Cache.files );
       }
