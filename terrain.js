b250109e04c5d0d71e980bd7be7df7bef3e750ed
AsherSalomon <AsherSalomon@gmail.com>
2022-06-17 23:03:38 -0400

    Update terrain.js

--- terrain.js
+++ terrain.js
@@ -221,10 +221,13 @@ class Tile {
   }
 
   pointIsInTile( x, z ) {
-    let a = x
-    let deltaX = ( x - this.reusedMesh.position.x ) / this.reusedMesh.width;
-    let deltaZ = ( z - this.reusedMesh.position.z ) / this.reusedMesh.width;
-    return Math.abs( deltaX ) < 0.5 && Math.abs( deltaZ ) < 0.5;
+    if ( this.reusedMesh.position != undefined ) {
+      let deltaX = ( x - this.reusedMesh.position.x ) / this.reusedMesh.width;
+      let deltaZ = ( z - this.reusedMesh.position.z ) / this.reusedMesh.width;
+      return Math.abs( deltaX ) < 0.5 && Math.abs( deltaZ ) < 0.5;
+    } else {
+      return false;
+    }
   }
 
   lookupData( x, z ) {
