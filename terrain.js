c3b1e69521b070726e25de8b2d3f39d98a6ac249
AsherSalomon <AsherSalomon@gmail.com>
2022-06-19 21:47:22 -0400

    Update terrain.js

--- terrain.js
+++ terrain.js
@@ -320,12 +320,12 @@ class ReusedMesh {
         let j = ( m * ( downSize + 1 ) + n ) * 3;
         let x = this.centerX + this.width * ( n / downSize - 0.5 );
         let z = this.centerZ + this.width * ( m / downSize - 0.5 );
-        // if ( this.clampingLayer != null ) {
-        //   vertices[ j + 1 ] = this.clampingLayer.lookupData( x, z );
-        // } else {
-        //   vertices[ j + 1 ] = 0;
-        // }
-        vertices[ j + 1 ] = 0;
+        if ( this.clampingLayer != null ) {
+          vertices[ j + 1 ] = this.clampingLayer.lookupData( x, z );
+        } else {
+          vertices[ j + 1 ] = 0;
+        }
+        // vertices[ j + 1 ] = 0;
         vertices[ j + 1 ] -= curvatureOfTheEarth( x, z );
       }
     }
