18b413676cf8e27a493c78e1b9547f9c840ff77c
AsherSalomon <AsherSalomon@gmail.com>
2022-06-22 08:53:08 -0400

    Update terrain.js

--- terrain.js
+++ terrain.js
@@ -96,13 +96,13 @@ class Square {
   }
 
   update() {
-    // if ( this.visible ) {
+    if ( this.visible ) {
       if ( this.zoom < maxZoom && this.isTooBig() ) {
         this.split();
       } else if ( this.zoom > minZoom && this.allSiblingsSmall() ) {
         this.parent.merge();
       }
-    // }
+    }
   }
 
   makeVisible() {
