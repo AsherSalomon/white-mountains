c85d1a27961debd9d67ef0d531019fc576bec918
AsherSalomon <AsherSalomon@gmail.com>
2022-06-24 08:17:48 -0400

    Update terrain.js

--- terrain.js
+++ terrain.js
@@ -87,7 +87,7 @@ export function update() {
           if ( generatorQueue[ i ].intendedSquare.visible = false ) {
             generatorQueue.splice( i, 1 );
           } else if ( generatorQueue[ i ].next().done ) {
-            // generatorQueue.splice( i, 1 ); // wtf inhibits merge
+            generatorQueue.splice( i, 1 ); // wtf inhibits merge
           }
           breakOut = true;
           break;
