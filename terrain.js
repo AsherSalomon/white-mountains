b14a501b444f7334acf79be4475a82de5ddd5bc9
AsherSalomon <AsherSalomon@gmail.com>
2022-06-24 08:07:04 -0400

    Update terrain.js

--- terrain.js
+++ terrain.js
@@ -119,7 +119,7 @@ class Square {
 
   update() {
     if ( this.distanceFromCamera() == 0 ) {
-      console.log( 'update' );
+      console.log( this.isTooBig() );
     }
     if ( this.zoom < maxZoom && this.isTooBig() && this.visible ) {
       this.split();
