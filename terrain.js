d6a772c33f36dda300a0a24fc78833393d0e05e0
AsherSalomon <AsherSalomon@gmail.com>
2022-06-23 23:30:04 -0400

    Update terrain.js

--- terrain.js
+++ terrain.js
@@ -82,11 +82,11 @@ export function update() {
     let breakOut = false;
     for ( let i = 0; i < generatorQueue.length; i++ ) {
       if ( generatorQueue[ i ].zoom == zoom ) {
-        // if ( generatorQueue[ i ].intendedSquare.visible = false ) {
-        //   generatorQueue.splice( i, 1 );
+        if ( generatorQueue[ i ].intendedSquare.visible = false ) {
+          generatorQueue.splice( i, 1 );
         // } else if ( generatorQueue[ i ].next().done ) {
         //   generatorQueue.splice( i, 1 );
-        // }
+        }
         breakOut = true;
         break;
       }
