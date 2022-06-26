e1d5733397ba70a1a4e4d27abc52bb62a9593520
AsherSalomon <AsherSalomon@gmail.com>
2022-06-26 19:53:18 -0400

    Update terrain.js

--- terrain.js
+++ terrain.js
@@ -641,7 +641,6 @@ class ReusedMesh {
         adjacents.push( simpleConcat[i] );
       }
     }
-    console.log( simpleConcat.length +' - '+ adjacents.length +' = '+ ( simpleConcat.length - adjacents.length ) );
     for ( let i = 0; i < adjacents.length; i++ ) {
       if ( adjacents[i].square.reusedMesh != null ) {
         adjacents[i].square.reusedMesh.refreshMesh();
