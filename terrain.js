035aa6de4cc2d9b85796a9c9b59183e0bfaea2c2
AsherSalomon <AsherSalomon@gmail.com>
2022-06-27 23:48:08 -0400

    Update terrain.js

--- terrain.js
+++ terrain.js
@@ -18,7 +18,7 @@ const minZoom = 5;
 const terrainZoom = 12;
 const polygonReduction = 2;
 const maxZoom = terrainZoom + polygonReduction;
-const satilliteZoom = 3;
+const satilliteZoom = 1;
 const satiliteTilesWidth = 2 ** satilliteZoom;
 if ( maxZoom + satilliteZoom > 20 ) { console.error( 'maxZoom + satilliteZoom > 20' ); }
 
