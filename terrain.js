37eb9e69dddbbdb54d5f15a38b3cbf869789a749
AsherSalomon <AsherSalomon@gmail.com>
2022-06-28 23:23:11 -0400

    Update terrain.js

--- terrain.js
+++ terrain.js
@@ -883,24 +883,21 @@ class DataCopy {
       }
     }
 
-    // satiliteTilesWidth
-    // IMAGERY_TILE_SIZE
-
+    let size = IMAGERY_TILE_SIZE * satiliteTilesWidth; // this.imageData.width
     let sizeRatio = this.width / reusedMesh.width;
-    // this.imageData.width
 
     // position to place the image data in the destination canvas.
-    let dx = this.centerX - reusedMesh.centerX;
-    let dy = this.centerZ - reusedMesh.centerZ;
+    let dx = 0;
+    let dy = 0;
 
     // position of the top-left corner the image data will be extracted.
-    let dirtyX = 0;
-    let dirtyY = 0;
+    let dirtyX = ( this.centerX - reusedMesh.centerX ) / reusedMesh.width * size;
+    let dirtyY = ( this.centerZ - reusedMesh.centerZ ) / reusedMesh.width * size;
 
-    let size = IMAGERY_TILE_SIZE * satiliteTilesWidth;
     // size of the rectangle to be painted. Defaults to the width of the image data.
-    let dirtyWidth = size;
-    let dirtyHeight = size;
+    let dirtyWidth = size * sizeRatio;
+    let dirtyHeight = size * sizeRatio;
+
     reusedMesh.satilliteCtx.putImageData(
       this.imageData, dx, dy, dirtyX, dirtyY, dirtyWidth, dirtyHeight
     );
