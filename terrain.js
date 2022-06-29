cde8bb568f44ec151a855e9e0e43a1d0c5ecb281
AsherSalomon <AsherSalomon@gmail.com>
2022-06-29 00:14:52 -0400

    Update terrain.js

--- terrain.js
+++ terrain.js
@@ -903,8 +903,8 @@ class DataCopy {
     let dirtyY = ( this.centerZ - reusedMesh.centerZ ) / reusedMesh.width * size;
 
     // size of the rectangle to be painted. Defaults to the width of the image data.
-    let dirtyWidth = size * sizeRatio;
-    let dirtyHeight = size * sizeRatio;
+    let dirtyWidth = 2; // size * sizeRatio;
+    let dirtyHeight = 2; // size * sizeRatio;
 
     reusedMesh.satilliteCtx.putImageData(
       this.imageData, dx, dy, dirtyX, dirtyY, dirtyWidth, dirtyHeight
