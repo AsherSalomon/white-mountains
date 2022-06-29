10ae5efe2e4952b66fb4838f5c31557e71f0bd39
AsherSalomon <AsherSalomon@gmail.com>
2022-06-29 09:09:16 -0400

    Update terrain.js

--- terrain.js
+++ terrain.js
@@ -849,9 +849,12 @@ class ReusedMesh {
     let dirtyWidth = size / sizeRatio;
     let dirtyHeight = size / sizeRatio;
 
+    this.satilliteCtx.scale( sizeRatio );
     this.satilliteCtx.putImageData(
-      dataCopy.imageData, dx, dy, dirtyX, dirtyY, dirtyWidth, dirtyHeight
+      // dataCopy.imageData, dx, dy, dirtyX, dirtyY, dirtyWidth, dirtyHeight
+      dataCopy.imageData, dx, dy, dirtyX, dirtyY
     );
+    this.satilliteCtx.scale( 1 / sizeRatio );
 
     // let size = IMAGERY_TILE_SIZE * satiliteTilesWidth; // this.imageData.width
     // let sizeRatio = dataCopy.width / this.width;
