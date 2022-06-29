0af4b181727d0ec767663cc3f3947a6eb4722e1c
AsherSalomon <AsherSalomon@gmail.com>
2022-06-29 00:06:31 -0400

    Update terrain.js

--- terrain.js
+++ terrain.js
@@ -906,9 +906,11 @@ class DataCopy {
     let dirtyWidth = size * sizeRatio;
     let dirtyHeight = size * sizeRatio;
 
-    reusedMesh.satilliteCtx.putImageData(
-      this.imageData, dx, dy, dirtyX, dirtyY, dirtyWidth, dirtyHeight
-    );
+    reusedMesh.satilliteCtx.putImageData( this.imageData );
+
+    // reusedMesh.satilliteCtx.putImageData(
+    //   this.imageData, dx, dy, dirtyX, dirtyY, dirtyWidth, dirtyHeight
+    // );
     reusedMesh.mesh.material.map = reusedMesh.texture;
     reusedMesh.mesh.material.color = new THREE.Color();
     reusedMesh.mesh.material.needsUpdate = true;
